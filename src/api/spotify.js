7a15701ebd3efa474a9878ca5a35e60d4002cacf
Sanghak <k3hppk@gmail.com>
2021-07-08 22:15:39 +0900

    Hide spotify

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SpotifyConfig } from "./config";
+import { SpotifyConfig } from "../../config";
 
 async function getSpotify() {
   const token = await getToken();
